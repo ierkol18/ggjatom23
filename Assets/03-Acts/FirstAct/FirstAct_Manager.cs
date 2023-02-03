51b6ba87ffad5167aca32e3183161a52c651b1d2
Irmak Erkol <ierkol18@ku.edu.tr>
2023-02-03 23:23:27 +0300

    first anim done

--- Assets/03-Acts/FirstAct/FirstAct_Manager.cs
+++ Assets/03-Acts/FirstAct/FirstAct_Manager.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FirstAct_Manager : MonoBehaviour
 {
 
     [SerializeField] GameObject stageChooserGO;
+    [SerializeField] Button playButton;
+    [SerializeField] Animator animator;
 
     private void Start()
     {
-   
+        playButton.onClick.AddListener(onPlayButton);
+        animator.enabled = false;
+    }
+
+    public void onPlayButton()
+    {
+        animator.enabled = true;
     }
 
     public void onAnimationEnd()
