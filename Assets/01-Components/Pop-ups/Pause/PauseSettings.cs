f02ba1edb87f9b5fd6c58aa26174978ea87c5270
Irmak Erkol <ierkol18@ku.edu.tr>
2023-02-03 20:05:23 +0300

    pause update

--- Assets/01-Components/Pop-ups/Pause/PauseSettings.cs
+++ Assets/01-Components/Pop-ups/Pause/PauseSettings.cs
@@ -4,13 +4,14 @@ using UnityEngine.UI;
 public class PauseSettings : MonoBehaviour
 {
     [SerializeField] Sprite musicOn, musicOff;
-    [SerializeField] Button replayButton, musicButton;
+    [SerializeField] Button replayButton, musicButton, resumeButton;
   
     void Start()
     {
         musicButton.GetComponent<Image>().sprite = musicOn;
         replayButton.onClick.AddListener(onReplayButton);
         musicButton.onClick.AddListener(onMusicButton);
+        resumeButton.onClick.AddListener(onResumeButton);
     }
 
     void onMusicButton()
@@ -22,6 +23,9 @@ public class PauseSettings : MonoBehaviour
     {
         //TODO
     }
-
+    void onResumeButton()
+    {
+        //TODO
+    }
     
 }
