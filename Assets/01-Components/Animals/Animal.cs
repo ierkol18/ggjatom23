ace2528c05ee501f23528c37aae6b59791ac436f
baris-sinapli <43214847+baris-sinapli@users.noreply.github.com>
2023-02-04 10:25:15 +0300

    Assets imported & DoTween implemented

--- Assets/01-Components/Animals/Animal.cs
+++ Assets/01-Components/Animals/Animal.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -9,10 +10,20 @@ public class Animal : MonoBehaviour, IPointerClickHandler
     public AnimalData animalData;
     [SerializeField] Image image;
     [SerializeField] RectTransform rectT;
+    // --------------TWEENING-------------------- 
+    private Sequence _sizeTween;
+    private RectTransform _animalRT;
+    [SerializeField] private float _tweenDuration = 0.5f;
+    [SerializeField] private Vector2 _tweenSize = new Vector2(600f, 600f);
+    [SerializeField] private Vector2 _originalSize = new Vector2(500f, 500f);
+
+    // ------------------------------------------
 
     void Start()
     {
         image.sprite = animalData.sprite;
+        DOTween.Init(true, true, LogBehaviour.Verbose).SetCapacity(200, 10); // Dotween initialized for the first time to adding bounce effect when clicked.
+        _animalRT = GetComponent<RectTransform>(); // Getting the rect transform of the animal sprite.
     }
 
     public void OnPointerClick(PointerEventData eventData)
@@ -22,6 +33,7 @@ public class Animal : MonoBehaviour, IPointerClickHandler
             GameManager.instance.onClick();
             StartCoroutine(onObjectClicked());
         }
+        tweenAnimalImage();
     }
 
     IEnumerator onObjectClicked()
@@ -36,6 +48,15 @@ public class Animal : MonoBehaviour, IPointerClickHandler
 
         Destroy(ooc.gameObject);
 
+    }
+
+    private void tweenAnimalImage()
+    {
+        _animalRT.DOSizeDelta(_tweenSize, _tweenDuration)
+            .SetEase(Ease.OutCubic)
+            .OnStepComplete(() => _animalRT.DOSizeDelta(_originalSize, _tweenDuration).SetEase(Ease.InCubic));
+        
+        //    .SetLoops(1, LoopType.Yoyo);
     }
 
 }
