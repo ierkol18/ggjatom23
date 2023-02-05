c5481cf7b3fa84b49be02ebdfc8a318e3116255d
ayzek1 <ishak.ozturk@metu.edu.tr>
2023-02-05 13:19:09 +0300

    fix build settings

--- Assets/01-Components/EndPanelAssignment.cs
+++ Assets/01-Components/EndPanelAssignment.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEditor.Build.Content;
+//using UnityEditor.Build.Content;
 using TMPro;
 using Unity.VisualScripting;
 
